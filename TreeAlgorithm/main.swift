61d17f9aa8d0b167a954e6d14119ad40d581c7e6
wymany <wymany@wyman.local>
2019-05-23 22:40:47 +0800

    [update]添加了N叉树的前序遍历

--- TreeAlgorithm/main.swift
+++ TreeAlgorithm/main.swift
@@ -8,3 +8,51 @@
 
 import Foundation
 
+
+// N-ary Tree preOrder Traversal
+public class Node {
+    var val:Int
+    var childern:[Node]?
+    init(_ val:Int, _ children:[Node]? = nil) {
+        self.val = val
+        self.childern = children
+    }
+    
+    //循环模式前序遍历
+    func preroder(_ root:Node?) -> [Int] {
+        if root == nil {
+            return []
+        }
+        var list = [Int]()
+        var stack:[Node] = [Node]()
+        stack.append(root!)
+        while !stack.isEmpty {
+            let root = stack.popLast()!
+            list.append(root.val)
+            guard let children = root.childern  else {
+                return list
+            }
+            var i = children.count - 1
+            while i >= 0 {
+                stack.append(children[i])
+                i = i - 1
+            }
+        }
+        return list
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+/*
+ 589. N叉树的前序遍历
+ https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+ */
+
